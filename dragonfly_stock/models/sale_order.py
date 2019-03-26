a3b945d06a4f782907c8358f71a6faff60677512
Jigar A. (Odoo) <jam@odoo.com>
2019-03-26 15:51:48 -0700

    [FIX] code clenaup too many nested conditions

--- dragonfly_stock/models/sale_order.py
+++ dragonfly_stock/models/sale_order.py
@@ -11,27 +11,16 @@ class SaleOrder(models.Model):
 
     @api.onchange('is_auto_fill')
     def _onchange_auto_fill(self):
-        if self.is_auto_fill:
-            if self.picking_ids:
-                for pick in self.picking_ids:
-                    if pick.is_pick:
-                        for line in pick.move_line_ids_without_package:
-                            # call write to set the "To" location to the current one
-                            line.write({'location_dest_id': self.sub_location_id.id})
-        else:
-            if self.picking_ids:
-                for pick in self.picking_ids:
-                    if pick.is_pick:
-                        for line in pick.move_line_ids_without_package:
-                            # set the "To" Location to the original standard location
-                            line.write({'location_dest_id': pick.location_dest_id.id})
+        for pick in self.picking_ids.filtered(lambda pk: pk.is_pick and pick.state not in  ('done', 'cancel')):
+            for line in pick.move_line_ids_without_package:
+                if self.is_auto_fill and self.sub_location_id:
+                    line.location_dest_id = self.sub_location_id
+                else:
+                    line.location_dest_id = pick.location_dest_id
 
     @api.onchange('sub_location_id')
     def _onchange_sub_location(self):
-        if self.is_auto_fill:
-            if self.picking_ids:
-                for pick in self.picking_ids:
-                    if pick.is_pick:
-                        for line in pick.move_line_ids_without_package:
-                            # call write to set the "To" location to the current one
-                            line.write({'location_dest_id': self.sub_location_id.id})
+        if self.is_auto_fill and self.sub_location_id:
+            for pick in self.picking_ids.filtered(lambda pk: pk.is_pick and pick.state not in  ('done', 'cancel')):
+                for line in pick.move_line_ids_without_package:
+                    line.location_dest_id = self.sub_location_id
