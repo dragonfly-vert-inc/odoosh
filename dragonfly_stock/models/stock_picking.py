7443b4d77332a502487773ba4258f01fbcbc76a5
cic-odoo <cic@odoo.com>
2019-04-04 14:51:57 -0700

    [ADD][1946175] Handled the case of returns as type pick.

--- dragonfly_stock/models/stock_picking.py
+++ dragonfly_stock/models/stock_picking.py
@@ -2,12 +2,19 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import models, fields, api, _
 
+
 class Picking(models.Model):
     _inherit = ['stock.picking']
 
     sub_location_id = fields.Many2one(related="sale_id.sub_location_id", string="Location", store=True)
     is_auto_fill = fields.Boolean(related="sale_id.is_auto_fill", string="Location Auto-fill")
-    is_pick = fields.Boolean(related="picking_type_id.is_pick", string="Is Pick")
+    is_pick = fields.Boolean(compute="_compute_is_pick", string="Is Pick", store=True)
+
+    @api.multi
+    @api.depends('picking_type_id', 'picking_type_id.is_pick')
+    def _compute_is_pick(self):
+        for pick in self:
+            pick.is_pick = pick.picking_type_id.is_pick
 
 
 class PickingType(models.Model):
@@ -25,3 +32,17 @@ class StockMoveLine(models.Model):
         for ml in mls.filtered(lambda xmll: xmll.picking_id.is_auto_fill and xmll.picking_id.is_pick):
             ml.location_dest_id = ml.picking_id.sub_location_id
         return mls
+
+
+# Overwrite the return picking create method to differentiate between a return pick and a standard pick
+class ReturnPicking(models.TransientModel):
+    _inherit = ['stock.return.picking']
+
+    def _create_returns(self):
+        new_pick, pick_type = super(ReturnPicking, self)._create_returns()
+
+        for pick in self.env['stock.picking'].browse(new_pick):
+            for line in pick.move_line_ids_without_package:
+                line.write({'location_dest_id': pick.location_dest_id.id})
+            pick.is_pick = False
+        return new_pick, pick_type
