e4eca8ec8b00e6329fae2c7a210af87b50d615b7
dip-ergo <dip@ergo-ventures.com>
2019-07-08 18:21:15 +0600

    Document popup and new tab pdf opener

---
+++ syray_wo_documents/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Syray WO Documents',
+    'summary': 'Syray WO Documents Enhancement',
+    'website': 'http://www.ergo-ventures.com',
+    'version': '1.0',
+    'author': 'Ergo Ventures Ltd.',
+    'description': """
+WO Documents
+===============
+
+* [1938284]
+    -  This will add a button ('Documents') on the work order process screen that will open a new popup with the attachment screen of the product
+       that is selected on the related manufacturing order.
+
+    """,
+    'category': 'Custom Development',
+    'depends': ['dragonfly_mrp'],
+    'data': [
+        # security
+        # views
+        'views/mrp_workorder_document_view.xml',
+        'views/syray_attachment_preview_template.xml',
+        'views/syray_attachment_wizard_view.xml',
+        'views/mrp_document_syray.xml',
+        'views/backend_assets.xml',
+        # data
+
+    ],
+    'demo': [],
+    'qweb': ['static/src/xml/syray_attachment_preview_qweb.xml'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
