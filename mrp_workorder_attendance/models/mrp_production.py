57e6d5bbd27015d5ba54dc6260f74467f992711a
Arifur Rahman <arifur.rahman@ergo-ventures.com>
2019-08-01 13:41:09 +0600

    cost calculation updated to add worker cost

--- mrp_workorder_attendance/models/mrp_production.py
+++ mrp_workorder_attendance/models/mrp_production.py
@@ -23,3 +23,20 @@ class MRPProduction(models.Model):
             else:
                 raise UserError("Work Orders steps already performed, can not cancel.")
         return  self.action_cancel()
+
+    def _cal_price(self, consumed_moves):
+        """Set a price unit on the finished move according to `consumed_moves`.
+        """
+        super(MRPProduction, self)._cal_price(consumed_moves)
+        worker_cost = 0
+        finished_move = self.move_finished_ids.filtered(lambda x: x.product_id == self.product_id and x.state not in ('done', 'cancel') and x.quantity_done > 0)
+        if finished_move:
+            finished_move.ensure_one()
+            for work_order in self.workorder_ids:
+                duration = sum(work_order.worker_times.mapped('worked_hours'))
+                worker_cost += duration * work_order.workcenter_id.cost_hour_employee
+            if finished_move.product_id.cost_method in ('fifo', 'average'):
+                qty_done = finished_move.product_uom._compute_quantity(finished_move.quantity_done, finished_move.product_id.uom_id)
+                finished_move.price_unit += (worker_cost/ qty_done)
+                finished_move.value += worker_cost
+        return True
