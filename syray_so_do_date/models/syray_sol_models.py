639e8bcd5f142e2bc41f4b601bf269add32733c8
dip-ergo <dip@ergo-ventures.com>
2019-09-05 15:26:10 +0600

    changes for sol line editation

--- syray_so_do_date/models/syray_sol_models.py
+++ syray_so_do_date/models/syray_sol_models.py
@@ -2,11 +2,17 @@
 
 from odoo import models, fields, api
 from datetime import timedelta
+from dateutil import relativedelta
+
+class SPDModel(models.Model):
+    _inherit = 'stock.move'
+
+    date_expected_custom = fields.Datetime("Delivery Date");
 
 class SOLModel(models.Model):
     _inherit = 'sale.order.line'
 
-    sol_delivery_date = fields.Datetime('Delivery Date Sol')
+    #sol_delivery_date = fields.Datetime.from_string('Delivery Date Sol')
     date_expected = fields.Datetime('Delivery Date')
 
     sol_priority = fields.Selection([
@@ -15,18 +21,26 @@ class SOLModel(models.Model):
 
     sale_delay = fields.Float(related='product_id.product_tmpl_id.sale_delay');
 
-#     @api.multi
-#     def _prepare_procurement_values(self, group_id=False):
-#         res = super(SOLModel, self)._prepare_procurement_values(group_id)
-#         date_expected_do = fields.Datetime.from_string(self.date_expected) - timedelta(days=int(self.sale_delay))
-#         res.update({'date_expected': date_expected_do})
-#         return res
-#
-# class StockRuleInherit(models.Model):
-#     _inherit = 'stock.rule'
-#
-#     def _get_stock_move_values(self, product_id, product_qty, product_uom, location_id, name, origin, values, group_id):
-#         res = super(StockRuleInherit, self)._get_stock_move_values(product_id, product_qty, product_uom, location_id,
-#                                                                    name, origin, values, group_id)
-#         res['date_expected'] = values.get('date_expected', False)
-#         return res
+    @api.multi
+    def _prepare_procurement_values(self, group_id=False):
+        res = super(SOLModel, self)._prepare_procurement_values(group_id)
+        date_expected_do = fields.Datetime.from_string(self.date_expected) - timedelta(days=int(self.sale_delay))
+        res.update({'date_expected_custom': date_expected_do})
+        return res
+    #
+    # @api.multi
+    # def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
+    #     res = super(SOLModel, self)._prepare_move_line_vals(quantity, reserved_quant)
+    #     date_expected_do = fields.Datetime.from_string(self.date_expected) - timedelta(days=int(self.sale_delay))
+    #     res.update({'date_expected': date_expected_do})
+    #     return res
+
+
+class StockRuleInherit(models.Model):
+        _inherit = 'stock.rule'
+
+        def _get_stock_move_values(self, product_id, product_qty, product_uom, location_id, name, origin, values, group_id):
+            res = super(StockRuleInherit, self)._get_stock_move_values(product_id, product_qty, product_uom, location_id,
+                                                                       name, origin, values, group_id)
+            res['date_expected_custom'] = values.get('date_expected_custom', False)
+            return res
--- syray_so_do_date/models/syray_spd_models.py
+++ syray_so_do_date/models/syray_spd_models.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
-from datetime import timedelta
-from odoo.tools import float_round
-import logging
-_logger = logging.getLogger(__name__)
-
-class SPDModel(models.Model):
-    _inherit = 'stock.move'
-
-    delivery_date = fields.Datetime('Delivery Date');
-    date_expected_sol = fields.Datetime(related='sale_line_id.date_expected');
-    sale_delay = fields.Float(related='product_id.product_tmpl_id.sale_delay');
+# from odoo import models, fields, api, _
+# from datetime import timedelta
+# from odoo.tools import float_round
+# import logging
+# _logger = logging.getLogger(__name__)
+#
+# class SPDModel(models.Model):
+#     _inherit = 'stock.move'
+#
+#     date_expected = fields.Datetime(related='sale_line_id.date_expected');
+#     date_expected_sol = fields.Datetime(related='sale_line_id.date_expected');
+#     sale_delay = fields.Float(related='product_id.product_tmpl_id.sale_delay');
