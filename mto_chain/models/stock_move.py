307f3507739142bf49dc0fc5721ea40764360ccc
Arifur Rahman <arifur.rahman@ergo-ventures.com>
2019-09-16 14:07:14 +0600

    added move parent mto field

--- mto_chain/models/stock_move.py
+++ mto_chain/models/stock_move.py
@@ -9,25 +9,19 @@ from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 
 
-class StockRule(models.Model):
-    _inherit = 'stock.rule'
+class StockMove(models.Model):
+    _inherit = 'stock.move'
 
-    def _run_manufacture(self, product_id, product_qty, product_uom, location_id, name, origin, values):
-        res = super(StockRule, self)._run_manufacture(product_id, product_qty, product_uom, location_id, name, origin, values)
-        origin_move = values.get('move_dest_ids') and values['move_dest_ids'][0]
-        production_id = origin_move.created_production_id
-        if production_id:
-            while origin_move:
-                if origin_move.sale_line_id:
-                    origin_record = origin_move.sale_line_id
-                elif origin_move.raw_material_production_id: 
-                    origin_record = origin_move.raw_material_production_id
-                    origin_move.need_to_procure = False
-                else:
-                    origin_record = False
-                if origin_record:
-                    production_id.node_id.write({'parent_ids': [(6,False,origin_record.node_id.ids)]})
-                    break
-                else:
-                    origin_move = origin_move.move_dest_ids[0] if origin_move.move_dest_ids else origin_move.move_dest_ids
-        return res
+    raw_mto_parent = fields.Many2one(
+        string=u'MTO Parent',
+        comodel_name='sale.order.line',
+        compute='_compute_mto_parent',
+    )
+    
+    @api.depends('raw_material_production_id')
+    def _compute_mto_parent(self):
+        for move in self:
+            if move.raw_material_production_id.node_id:
+                raw_mto_parent = move.raw_material_production_id.node_id._get_parent()
+                if raw_mto_parent.res_model == 'sale.order.line':
+                    move.raw_mto_parent = raw_mto_parent.record_ref
