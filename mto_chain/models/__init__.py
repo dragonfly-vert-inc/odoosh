c9eb78597c63859598fcc7b5a96f8cc861f414c4
Arifur Rahman <arifur.rahman@ergo-ventures.com>
2019-09-23 17:25:39 +0600

    minor

--- mto_chain/models/__init__.py
+++ mto_chain/models/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import mto_priority
 from . import mto_chain
 from . import stock_move
 from . import stock_rule
-from . import mto_priority
 from . import inherit
 from . import mrp
 from . import purchase_link
