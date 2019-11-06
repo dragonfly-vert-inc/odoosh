a688e10774624a9e46184675d337a52a29b33930
Arifur Rahman <arifur.rahman@ergo-ventures.com>
2019-11-06 19:26:37 +0600

    added new procurement  linking

--- mto_chain/models/purchase_link.py
+++ mto_chain/models/purchase_link.py
@@ -9,11 +9,12 @@ from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 
 
-class PoMoLink(models.TransientModel):
+class PoMoLink(models.Model):
     _name = 'procurement.linking'
     _description = u'Procurement Raw Material Consumption Linking'
+    _rec_name = 'purchase_id'
 
-
+    purchase_id = fields.Many2one(string=u'Purchase',comodel_name='purchase.order',ondelete='set null')
     line_ids = fields.One2many(comodel_name='procurement.linking.line',inverse_name='linking_id',)
     linked = fields.Boolean(default=False)
     
@@ -22,41 +23,55 @@ class PoMoLink(models.TransientModel):
     def link_manual_procurement(self):
         for linking in self:
             for line in linking.line_ids:
-                if not line.production_id.node_id:
-                    line.production_id.node_id = line.production_id.node_id.create({})
-                    line.production_id.node_id._set_ref(line.production_id)
+                parent_record = line.production_id if line.production_id else line.sale_id
+                if not parent_record.node_id:
+                    parent_record.node_id = parent_record.node_id.create({})
+                    parent_record.node_id._set_ref(parent_record)
                 if not line.purchase_id.node_id:
                     line.purchase_id.node_id = line.purchase_id.node_id.create({})
                     line.purchase_id.node_id._set_ref(line.purchase_id)
-                line.production_id.node_id.write({
+                parent_record.node_id.write({
                     'child_ids': [(4, line.purchase_id.node_id.id, False)]
                 })
+                moves = line.production_id.move_raw_ids.filtered(lambda m: m.product_id == line.purchase_id.product_id) if line.production_id else line.sale_id.move_ids
+                for move in moves:
+                    while(move.move_orig_ids):
+                        move=move.move_orig_ids
+                    move.created_purchase_line_id = line.purchase_id
             linking.linked = True
 
-class PoMoLinkingLine(models.TransientModel):
+class PoMoLinkingLine(models.Model):
     _name = 'procurement.linking.line'
     _description = u'Procurement Raw Material Consumption Linking Line'
 
-    
-    purchase_id = fields.Many2one(
-        string=u'Purchase',
-        comodel_name='purchase.order',
-        ondelete='set null',
-    )
-    production_id = fields.Many2one(
-        string=u'Production',
-        comodel_name='mrp.production',
-        ondelete='set null',
-    )
-    
-    linking_id = fields.Many2one(
-        comodel_name='procurement.linking',
-        ondelete='set null',
-    )
-    
+    linking_id = fields.Many2one(comodel_name='procurement.linking',ondelete='set null')    
+    purchase_id = fields.Many2one(string=u'Purchase',comodel_name='purchase.order.line',ondelete='set null')
+    production_id = fields.Many2one(string=u'Production',comodel_name='mrp.production',ondelete='set null')
+    sale_id = fields.Many2one(string=u'Sale Line',comodel_name='sale.order.line',ondelete='set null')
+    supply_quantity = fields.Float(related='purchase_id.product_uom_qty')
+    supply_uom = fields.Many2one(string=u'Supply Unit',comodel_name='uom.uom',related='purchase_id.product_uom')
+    demand_quantity = fields.Float(string="Demand Quantity", compute='_get_demand')
+    demand_uom = fields.Many2one(string=u'Demand Unit',comodel_name='uom.uom',compute='_get_demand')
+    from_stock = fields.Boolean()
+    source = fields.Char(compute='_get_source')
+
+    def _get_source(self):
+        for record in self:
+            if record.production_id.node_id:
+                record.source = record.production_id.node_id._get_parent().display_name
+
+
+    def _get_demand(self):
+        for record in self:
+            if record.purchase_id and record.production_id:
+                record.demand_quantity = sum(record.production_id.move_raw_ids.filtered(lambda m: m.product_id == record.purchase_id.product_id).mapped('product_uom_qty'))
+                record.demand_uom = record.production_id.move_raw_ids.filtered(lambda m: m.product_id == record.purchase_id.product_id).mapped('product_uom')
+            elif record.purchase_id and record.sale_id:
+                record.demand_quantity = sum(record.sale_id.move_ids.mapped('product_uom_qty'))
+                record.demand_uom = record.sale_id.move_ids.mapped('product_uom')
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = 'purchase.order.line'
 
     @api.multi
     def link_manufactures(self):
